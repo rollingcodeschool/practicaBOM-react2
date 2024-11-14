14608a58c8a8abf909dc174020341dd537f200ad
Emilse Arias <emilsearias@gmail.com>
2024-11-14 10:09:08 -0300

    corregimos las horas, minutos y segundos de dos digitos

--- js/reloj.js
+++ js/reloj.js
@@ -1,28 +1,40 @@
-function obtenerFecha(){
-    const fecha = new Date()
-    // console.log(fecha.getDate()) // devuelve el numero del día del mes
-    // console.log(fecha.getDay()) // 0 - 6, siendo 0 el domingo
-    // console.log(fecha.getMonth())
-    // console.log(fecha.getFullYear())
-    // console.log(fecha.getMinutes())
-    // console.log(fecha.getHours())
-    // console.log(fecha.getSeconds())
-    const diasSemana= ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
-    const meses = ['Enero', 'Febrero','M','A','M','J','J','A','S','O','Noviembre', 'Diciembre']
-    
-    // console.log(diasSemana[fecha.getDay()])
-    // console.log(meses[fecha.getMonth()])
-    
-    const textoFecha = `${diasSemana[fecha.getDay()]} ${fecha.getDate()} de ${meses[fecha.getMonth()]} de ${fecha.getFullYear()}`
-    // console.log(textoFecha)
-    
-    //buscar el h2 que tiene la fecha
-    const tituloFecha = document.querySelector('h2');
-    const reloj = document.querySelector('h3');
-    // console.log(tituloFecha)
-    
-    tituloFecha.innerText = textoFecha
-    reloj.innerHTML = `${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`
+function obtenerFecha() {
+  const fecha = new Date();
+  const diasSemana = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+  const meses = [
+    "Enero",
+    "Febrero",
+    "M",
+    "A",
+    "M",
+    "J",
+    "J",
+    "A",
+    "S",
+    "O",
+    "Noviembre",
+    "Diciembre",
+  ];
+  const textoFecha = `${diasSemana[fecha.getDay()]} ${fecha.getDate()} de ${
+    meses[fecha.getMonth()]
+  } de ${fecha.getFullYear()}`;
+
+  const tituloFecha = document.querySelector("h2");
+  const reloj = document.querySelector("h3");
+
+  tituloFecha.innerText = textoFecha;
+  const horas = fecha.getHours().toString().padStart(2, "0");
+  const minutos = fecha.getMinutes().toString().padStart(2, "0");
+  const segundos = fecha.getSeconds().toString().padStart(2, "0");
+  reloj.innerHTML = `${horas}:${minutos}:${segundos}`;
 }
 
-setInterval(obtenerFecha,1000)
+setInterval(obtenerFecha, 1000);
